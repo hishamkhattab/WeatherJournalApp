e4e4650651efbce94f0790164b2c3edb17706ac5
Hisham Khattab <42070333+hishamkhattab@users.noreply.github.com>
2021-12-07 20:36:28 +0200

    fetching data from openWeatherMap API

--- website/app.js
+++ website/app.js
@@ -1,5 +1,33 @@
 /* Global Variables */
+let myData = {};
+// api related variables
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+const apiKey = '524a9b4b2764750f03c78efa38d6d608';
+
+// UI element
+const zipCode = document.getElementById('zip');
+const feelingText = document.getElementById('feeling');
+const generateBtn = document.getElementById('generate');
+
+// content to update
+const content = document.getElementById('entryHolder');
 
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = new Date();
+let date = newDate.toLocaleString().split(',')[0];
+
+
+/** fetch data from OpenWeatherMap API*/
+const getWeather = async (url, zip, key) => {
+    
+    const base = `${url}?zip=${zip}&units=metric&appid=${key}`
+    
+    const response = await fetch(base);
+    const data = await response.json();
+
+    //return the tempreture in celesuis
+    console.log(data);
+    return data.main.temp;
+};
+
+getWeather(baseUrl,'14202', apiKey);
